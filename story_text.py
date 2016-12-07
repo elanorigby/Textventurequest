aba1ff8b55fc9cd9d1b9b12f1b8a6eebbc86195b
Sharlak <elanorigby@gmail.com>
2016-12-07 14:53:17 +0000

    updated slowth() to pause at commas and periods

--- story_text.py
+++ story_text.py
@@ -1,22 +1,43 @@
 import time
+import re
 from random import uniform
+
+# def slowth(string):
+#     counter = 0
+#     for letter in string:
+#         if counter >= 90:
+#             print(letter)
+#             counter = 0
+#         else:
+#             print(letter, end='', flush=True)
+#             time.sleep(uniform(0, 0.1))
+#             counter += 1
+#
+#     return ' \n'
+
 
 def slowth(string):
     counter = 0
-    for word in string.split():
-        if counter >= 20:
-            print(word)
+    for letter in string:
+        if re.findall(r'[,\.\s]', letter) and counter >= 100:
+            print(letter)
             counter = 0
+        elif re.findall(r'[,\.]', letter):
+            print(letter, end='', flush=True)
+            time.sleep(uniform(0.4, 0.6))
+            counter += 1
         else:
-            print(word, end=' ', flush=True)
-            time.sleep(uniform(0, 0.5))
+            print(letter, end='', flush=True)
+            time.sleep(uniform(0, 0.1))
             counter += 1
 
+    return '\n'
 
 
-intro = slowth("The gates of the city loom up out of the early morning mist rising from the river. You step off the boat and " \
+
+intro = "The gates of the city loom up out of the early morning mist rising from the river. You step off the boat and " \
           "walk down the wharf through the great stone archway and into the city. The people here do not look like you, " \
-          "and you pull your hood up over your head, keenly aware of your difference. As you do so, a voice suddenly calls out -")
+          "and you pull your hood up over your head, keenly aware of your difference. As you do so, a voice suddenly calls out - \n"
 
 journey_to_door = "You turn to look back towards the city gates, and when you turn again to face your advisor, she is gone. " \
                   "\"Of course she's gone,\" you think. \"And it'll bite me in the ass later that I didn't thank her.\" But " \
@@ -27,5 +48,4 @@ journey_to_door = "You turn to look back towards the city gates, and when you tu
                   "On it is an inscription:"
 
 
-
-# print(slowth("There is a house in new orleans"))
+print(slowth(intro))
