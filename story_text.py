e24b1182df97524b2819d1bdf4022d4ab8d1ad6b
Sharlak <elanorigby@gmail.com>
2016-12-08 17:04:38 +0000

    changed 'intro' to 'arrival'

--- story_text.py
+++ story_text.py
@@ -1,7 +1,4 @@
-
-
-
-intro = "The gates of the city loom up out of the early morning mist rising from the river. You step off the boat and " \
+arrival = "The gates of the city loom up out of the early morning mist rising from the river. You step off the boat and " \
           "walk down the wharf through the great stone archway and into the city. The people here do not look like you, " \
           "and you pull your hood up over your head, keenly aware of your difference. As you do so, a voice suddenly calls out - "
 
@@ -13,7 +10,10 @@ journey_to_door = "You turn to look back towards the city gates, and when you tu
                   "streaming in and out of the gates, is a small wooden door. It is set into the wall a mere 10 feet from the left gate. " \
                   "On it is an inscription:"
 
-through_the_door = "You hesitate and glance around quickly. No one is looking at you. It almost seems unnatural, but you" \
-                   "shrug it off and push slightly at the door. It swings smoothly, silently inward. Behind it is a darkness" \
+through_the_door = "You hesitate and glance around quickly. No one is looking at you. It almost seems unnatural, but you " \
+                   "shrug it off and push slightly at the door. It swings smoothly, silently inward. Behind it is a darkness " \
                    "that the broadening morning light does nothing to illuminate. You turn your face to the sun for a moment, " \
                    "feeling its heat, and plunge in. \n" \
+
+
+temp_ending = "And that's all she wrote............. for now"
